46875bcf3b5478a51fb676b8bfbe20c7ab98db70
jclairet <sangoku0516@hotmail.fr>
2017-01-20 21:28:55 +0100

    seed des rôles intranet

--- database/seeds/RolesTableSeeder.php
+++ database/seeds/RolesTableSeeder.php
@@ -10,8 +10,38 @@ class RolesTableSeeder extends Seeder
         DB::table('roles')->truncate();
         Roles::create([
             'id'            => 1,
-            'name'          => 'Root',
+            'name'          => 'Technique',
             'description'   => 'Use this account with extreme caution. When using this account it is possible to cause irreversible damage to the system.'
+        ]);
+	Roles::create([
+            'id'            => 2,
+            'name'          => 'Administrateur',
+            'description'   => 'Description du rôle Administrateur'
+        ]);
+	Roles::create([
+            'id'            => 3,
+            'name'          => 'Superviseur',
+            'description'   => 'Description du rôle Superviseur'
+        ]);
+	Roles::create([
+            'id'            => 4,
+            'name'          => 'Manager',
+            'description'   => 'Description du rôle Manager'
+        ]);
+	Roles::create([
+            'id'            => 5,
+            'name'          => 'RH',
+            'description'   => 'Description du rôle RH'
+        ]);
+	Roles::create([
+            'id'            => 6,
+            'name'          => 'Collaborateur',
+            'description'   => 'Description du rôle Collaborateur'
+        ]);
+	Roles::create([
+            'id'            => 7,
+            'name'          => 'Rédacteur',
+            'description'   => 'Description du rôle Rédacteur'
         ]);
     }
 }
