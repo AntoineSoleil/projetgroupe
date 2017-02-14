b65808c8fa30622e9321a7716346a673f4f1fb28
yoannduc <dev.yoannduc@gmail.com>
2017-02-14 16:24:35 +0100

    Ajout du second script

--- init.sh
+++ init.sh
@@ -9,8 +9,4 @@ sudo service mysql restart
 sudo wget https://apt.dockerproject.org/repo/pool/main/d/docker-engine/docker-engine_1.9.1-0~jessie_amd64.deb -P /tmp/
 sudo dpkg -i /tmp/docker-engine_1.9.1-0~jessie_amd64.deb
 sudo docker build -t php7 -f Dockerfile.php7 .
-#cd ~/Documents
-#git clone $1 && cd projetgroupe
 sudo docker run -dit -p 80:80 -v $(pwd):/var/www/html/ --name php7-test php7
-#chmod 755 -R ../projetgroupe
-#chmod -R o+w storage
